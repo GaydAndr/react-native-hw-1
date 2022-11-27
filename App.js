dde55e68f7edcbc70be275f6a127c8ac4846c8d1
andrii_h <galgayand@gmail.com>
2022-11-28 00:54:25 +0200

    clear app

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { useState, useEffect, useCallback } from 'react';
-import {
-  StyleSheet,
-  View,
-  Dimensions,
-  BackHandler,
-  Alert,
-  StatusBar,
-} from 'react-native';
-
+import { View, Alert, BackHandler } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-
-import { RegistrationScreen } from './src/components/RegistrationScreen/RegistrationScreen';
-import { LoginScreen } from './src/components/LoginScreen/LoginScreen';
-
-const statusBarHeight = StatusBar.currentHeight;
+import { useRoute } from './src/router';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'Roboto-Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
     'Roboto-Medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
   });
-  const [registerData, setRegisterData] = useState([]);
-  console.log(registerData);
-
-  const [dimensions, setDimensions] = useState({
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  });
+  const routing = useRoute(true);
 
   useEffect(() => {
     async function prepare() {
@@ -35,23 +18,23 @@ export default function App() {
     }
     prepare();
 
-    const backAction = () => {
-      Alert.alert('Hold on!', 'Are you sure you want to go out?', [
-        {
-          text: 'Cancel',
-          onPress: () => null,
-          style: 'cancel',
-        },
-        { text: 'YES', onPress: () => BackHandler.exitApp() },
-      ]);
-    };
+    // const backAction = () => {
+    //   Alert.alert('Hold on!', 'Are you sure you want to go out?', [
+    //     {
+    //       text: 'Cancel',
+    //       onPress: () => null,
+    //       style: 'cancel',
+    //     },
+    //     { text: 'YES', onPress: () => BackHandler.exitApp() },
+    //   ]);
+    // };
 
-    const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      backAction
-    );
+    // const backHandler = BackHandler.addEventListener(
+    //   'hardwareBackPress',
+    //   backAction
+    // );
 
-    return () => backHandler.remove();
+    // return () => backHandler.remove();
   }, []);
 
   const onLayoutRootView = useCallback(async () => {
@@ -63,12 +46,11 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
+
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <RegistrationScreen setRegisterData={setRegisterData} />
-      {/* <LoginScreen setRegisterData={setRegisterData} /> */}
-    </View>
+    <>
+      <View onLayout={onLayoutRootView}></View>
+      <NavigationContainer>{routing}</NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
